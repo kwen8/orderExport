b475b3a05fb65aeabc4f6702399bde006c878491
chankwen <418415881@qq.com>
2018-02-09 20:04:58 +0800

    init project

---
+++ src/renderer/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'order-export'
+  }
+</script>
+
+<style lang="scss">
+  html, body {
+    width: 100%;
+    height: 100%;
+  }
+  #app {
+    width: 100%;
+    height: 100%;
+  }
+</style>
---
+++ src/renderer/pages/Login.vue
@@ -0,0 +1,44 @@
+<template>
+  <Row type="flex" justify="center" align="middle" class="login__layout">
+    <Col>
+      <Card style="width:350px">
+        <p slot="title">
+          <Icon type="ios-film-outline"></Icon>
+          登录
+        </p>
+        <Form ref="formCustom" :model="loginForm" :rules="ruleCustom" :label-width="80">
+          <FormItem label="用户名" prop="user">
+            <Input type="text" v-model="loginForm.user" />
+          </FormItem>
+          <FormItem label="密码" prop="password">
+            <Input type="password" v-model="loginForm.password" />
+          </FormItem>
+          <FormItem>
+            <Button type="primary" @click="handleSubmit('loginForm')">登录</Button>
+            <Button type="ghost" @click="handleReset('loginForm')" style="margin-left: 8px">重置</Button>
+          </FormItem>
+        </Form>
+      </Card>
+    </Col>
+  </Row>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        user: '',
+        password: ''
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scope>
+  .login__layout {
+    height: 100%;
+  }
+</style>
+
